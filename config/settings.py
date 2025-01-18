2d4da68e36928e042b5de2f72aa224af6f6d4225
yeonjae <pyj3740@gmail.com>
2025-01-18 16:00:27 +0900

    update allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,11 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "3.35.205.226",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
